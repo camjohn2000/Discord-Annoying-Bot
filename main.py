e33cd54de07f455f29db87a8904ab4110ac027a3
Cameron Johnson <55406957+camjohn2000@users.noreply.github.com>
2021-09-11 22:23:31 +0000

    Bot will now send non-stop dad jokes until prompted with stop command

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+""""
+First attempt at making a discord bot
+
+Annoyingly sends non-stop dad jokes to test channel when someone types anything
+
+Cameron Johnson
+9-10-2021
+""""
+
 import discord
 import os
 import json
 import requests
+from discord.ext import commands, tasks
 
 client = discord.Client()
 
-def get_joke():
-  pass
+@tasks.loop(seconds=2)
+async def get_joke():
+  #find the channel the user is in
+  channel = client.get_channel('CHANNEL_ID_HERE')
+ 
+ #dad joke from API icanhazdadjoke, sent every 2 seconds in text channel
+  joke = requests.get('https://icanhazdadjoke.com', headers={"Accept": "text/plain"}).text
+  await channel.send(joke)
+  
 
 #console logging that bot is logged in and online
 @client.event
@@ -24,6 +41,13 @@ async def on_message(msg):
   #bot replies when it reads command in text channel
   if msg.content.startswith("%hey bot"):
     await msg.channel.send("HIIIIIIII")
+  
+  #stops the annoying loop
+  elif msg.content.startswith("%chill"):
+    get_joke.stop()
+    await msg.channel.send("lmao ok")
+  else:
+    get_joke.start()
 
 #token defined privately
 client.run(os.environ['TOKEN'])
