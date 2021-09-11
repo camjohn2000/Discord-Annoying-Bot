eb087621ffaa1f12fa8428bbd4fde466fa20ced3
Cameron Johnson <55406957+camjohn2000@users.noreply.github.com>
2021-09-11 04:45:16 +0000

    basic bot with singular command added

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+import os
+import requests
+
+client = discord.Client()
+
+def get_joke():
+  pass
+
+#console logging that bot is logged in and online
+@client.event
+async def on_ready():
+  print("Logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(msg):
+  
+  #ignores it's own messages
+  if msg.author == client.user:
+    return
+  
+  #bot replies when it reads command in text channel
+  if msg.content.startswith("%hey bot"):
+    await msg.channel.send("HIIIIIIII")
+
+#token defined privately
+client.run(os.environ['TOKEN'])
+
+
